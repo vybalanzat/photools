e0d1ebe7f147de4968420121a5da4c17a5227a6a
Vy Balanzat <vybalanzat@gmail.com>
2019-10-13 20:32:31 -0400

    updates git status

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+// Var for Random Number:
+
+
+    // Create variable for wins and losses
+
+    var wins = 0;
+    var losses = 0;
+    var totalNumber = 0;
+    var purple = 0;
+    var blue = 0;
+    var green = 0;
+    var red = 0;
+   
+
+    // Set Html where to display it
+
+    var userWins = document.getElementById("wins-text");
+    var userLosses = document.getAnimations("losses-text");
+    var userTotalNumber = document.getElementById("totalNumber-text");
+    var userRandomNumber = document.getElementById("randomNumber-text");
+
+$(document).ready(function) {
+    $("#button").on("click"(function () {
+        var randomNumber = Math.floor(Math.random() * 101);
+
+
+            
+
+
+        }
+
+
+
+
+
+    });
+
+
+
+
+
+    
